1fa8e89d895ee90263bab97ef1f39dab296337d6
Manoj <mj.manojoshi005@gmail.com>
2024-06-17 07:01:28 +0530

    neew

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,8 @@
-'use strict';
-
-
-
-/**
- * add event listener on multiple elements
- */
-
 const addEventOnElements = function (elements, eventType, callback) {
   for (let i = 0, len = elements.length; i < len; i++) {
     elements[i].addEventListener(eventType, callback);
   }
 }
-
-
-
-/**
- * MOBILE NAV
- */
-
 const navbar = document.querySelector("[data-navbar]");
 const navbarTogglers = document.querySelectorAll("[data-nav-toggler]");
 const navbarLinks = document.querySelectorAll("[data-nav-link]");
